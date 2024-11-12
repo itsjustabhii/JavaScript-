c1bf57cdda813000d914b0b967f151ecbf7b9eb0
Abhishek Harish <itsjustabhii@gmail.com>
2024-11-12 14:28:15 +0530

    functions

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ let person ={
 person.age = 27
 
 console.log(person)
+
+//Objects
+//objects are dynamic
+let colors = ['Red', 'white']
+colors[2] = 'Blue'
+console.log(colors)
+
+//Functions
+function greet(){
+    console.log("I like momos")
+}
+
+greet()
